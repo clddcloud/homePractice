b41e2ed41877a6cbc915e53d9e0078b1eaee5eda
clddcloud <artem.maximov@gmail.com>
2024-09-14 09:43:07 +0200

    14th commit

--- src/Homework2/E5.java
+++ src/Homework2/E5.java
@@ -5,12 +5,17 @@ public class E5 {
         //5. Check if Two Strings are Anagrams:
         // Given two strings, determine if they are anagrams, meaning they contain the same characters in a different order.
         // For example, "listen" and "silent" are anagrams.
+
+        // another much better approach below
+
         String str1 = "listen_silent_silsil";
         String str2 = "silent_listen_lislis";
         int count = 0;
         int doubeLetterCountStr1 = 0;
         int doubeLetterCountStr2 = 0;
         if (str1.length() == str2.length()) {
+            int sum1=0;
+            int sum2=0;
             for (int i = 0; i < str1.length(); i++) {
                 for (int j = 0; j < str2.length(); j++) {
                     if (str1.charAt(i) == str2.charAt(j)) {
@@ -25,14 +30,16 @@ public class E5 {
                     if (i != j && str2.charAt(i) == str2.charAt(j)) {
                         doubeLetterCountStr2++;
                     }
-
                 }
             }
+            System.out.println(sum1);
+            System.out.println(sum2);
             if (count == str1.length() && count != 0 && doubeLetterCountStr1 == doubeLetterCountStr2) {
                 System.out.println("Words: '" + str1 + "' & '" + str2 + "' are anagrams. True");
             } else {
                 System.out.println("They are NOT anagrams. False");
             }
+
 
             // Another option with more neat code and straight-forward logic:
 
@@ -41,15 +48,11 @@ public class E5 {
                 String str2tmp=str2;
                 for (int i = 0; i < str1.length(); i++) {
                     char currentChar = str1.charAt(i);
-                    // Find the index of the current character in str2
                     int indexInStr2 = str2tmp.indexOf(currentChar);
-                    // Check if currentChar exists in the str2
-                       if (indexInStr2 != -1) {
-                      // Remove the found character from str2
+                       if (indexInStr2 >= 0) {
                        str2tmp = str2tmp.replaceFirst(String.valueOf(currentChar),"");
                     }
                 }
-                // If all characters match and str2 is empty, they are anagrams
                 cond=str2tmp.isEmpty();
             }
             if (cond) {
